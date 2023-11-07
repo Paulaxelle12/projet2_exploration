adf9b778e05e880bcfe60db892c26d6cbe0ce27b
Tychette <93228104+Tychette@users.noreply.github.com>
2023-11-06 21:17:32 -0500

    Update converter.js

--- routes/api/converter.js
+++ routes/api/converter.js
@@ -5,110 +5,51 @@ router.get("/", (req, res) => {
     res.send("Need a POST request plz!");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
     // Extract type and value from the request body
     const { type, value } = req.body;
-    // Check if the type is "feet2meter" and the value is a valid number
-    if (type.toLowerCase() === "feet2meter" && !isNaN(value)) {
-        // log to server console for debugging
-        console.log("received data: " + req.body.type + " for " + req.body.value);
-        // Convert value from feet to meters (1 foot = 0.3048 meters)
-        const meters = parseFloat(value) * 0.3048;
-        // Log the received data and the converted value
-        console.log(`Received data: ${value} feet. Converted to ${meters} meters.`);
-        // Send the converted value in the response
-        res.send(`Converted value: ${meters} meters.`);
-    }
-    else if(type.toLowerCase() === "meters2feet" && !isNaN(value)){
-        // log to server console for debugging
-        console.log("received data: " + req.body.type + " for " + req.body.value);
-        //Conversion
-        const feet = metersToFeet(value);
-        // Log the received data and the converted value
-        console.log(`Received data: ${value} meters. Converted to ${feet} feet.`);
-        //Envoi
-        res.send(`Converted value: ${feet} feet.`);
-    }
-    else if(type.toLowerCase() === "kilotopounds" && !isNaN(value))
-    {
-        // log to server console for debugging
-        console.log("received data: " + req.body.type + " for " + req.body.value);
-        //Conversion
-        const pounds = kilogramsToPounds(value);
-        // Log the received data and the converted value
-        console.log(`Received data: ${value} kilograms. Converted to ${pounds} pounds.`);
-        //Envoi
-        res.send(`Converted value: ${pounds} pounds.`);
-    }
-    else {
-        // If the type is defined or the value is not a valid number, send an error response
-        res.status(400).send("Invalid input.");
-    }
-  });
 
-module.exports = router;
+    // Validation des données d'entrée
+    if (!type || !value || isNaN(value)) {
+        return res.status(400).json({ error: "Invalid input. Provide type and numeric value." });
+    }
 
-// Conversion de Longueur
+    // Conversion en minuscules pour gérer la casse
+    const conversionType = type.toLowerCase();
+
+    let result;
+    switch (conversionType) {
+        case "feet2meter":
+            result = feetToMeters(value);
+            break;
+        case "meters2feet":
+            result = metersToFeet(value);
+            break;
+        case "kilotopounds":
+            result = kilogramsToPounds(value);
+            break;
+        default:
+            return res.status(400).json({ error: "Invalid conversion type." });
+    }
+
+    // Log de la conversion
+    console.log(`Received data: ${value} for ${type}. Converted to ${result}`);
+
+    // Envoi du résultat
+    res.json({ result });
+});
+
+// Fonctions de conversion (à conserver telles quelles)
 function metersToFeet(meters) {
-  return meters * 3.28084;
+    return meters * 3.28084;
 }
 
 function feetToMeters(feet) {
-  return feet / 3.28084;
+    return feet / 3.28084;
 }
 
-// Conversion de Poids
 function kilogramsToPounds(kilograms) {
-  return kilograms * 2.20462;
+    return kilograms * 2.20462;
 }
 
-function poundsToKilograms(pounds) {
-  return pounds / 2.20462;
-}
-
-// Conversion de Température
-function celsiusToFahrenheit(celsius) {
-  return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5/9;
-}
-
-// Conversion de Devise
-function bitcoinToCAD(bitcoin) {
-  // Remplacez cette valeur par le taux de change actuel
-  const exchangeRateBTCtoCAD = 50000; // Exemple seulement
-  return bitcoin * exchangeRateBTCtoCAD;
-}
-
-function CADToBitcoin(cad) {
-  // Remplacez cette valeur par le taux de change actuel
-  const exchangeRateCADtoBTC = 0.00002; // Exemple seulement
-  return cad * exchangeRateCADtoBTC;
-}
-
-// Conversion de Volume
-function litersToGallons(liters) {
-  return liters * 0.264172;
-}
-
-function gallonsToLiters(gallons) {
-  return gallons / 0.264172;
-}
-
-// Exemples d'utilisation
-console.log("5 mètres équivalent à " + metersToFeet(5) + " pieds.");
-console.log("10 pieds équivalents à " + feetToMeters(10) + " mètres.");
-
-console.log("2 kilogrammes équivalents à " + kilogramsToPounds(2) + " livres.");
-console.log("5 livres équivalent à " + poundsToKilograms(5) + " kilogrammes.");
-
-console.log("20 degrés Celsius équivalent à " + celsiusToFahrenheit(20) + " degrés Fahrenheit.");
-console.log("68 degrés Fahrenheit équivalents à " + fahrenheitToCelsius(68) + " degrés Celsius.");
-
-console.log("1 Bitcoin équivaut à " + bitcoinToCAD(1) + " dollars canadiens.");
-console.log("1000 dollars canadiens équivalent à " + CADToBitcoin(1000) + " Bitcoins.");
-
-console.log("10 litres équivalent à " + litersToGallons(10) + " gallons.");
-console.log("5 gallons équivalents à " + gallonsToLiters(5) + " litres.");
+module.exports = router;
