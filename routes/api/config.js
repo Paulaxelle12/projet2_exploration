23015ee16b800dd031584607c9a98ef330943ba6
Tychette <201940472@cegepsherbrooke.qc.ca>
2023-11-17 10:17:06 -0500

    Update

--- routes/api/config.js
+++ routes/api/config.js
@@ -1,7 +1,5 @@
 // config.js
 
-module.exports = {
-  //mongoURI: "mongodb+srv://Client:uZKWfX6J0aejgnst@atlascluster.2bzkbwe.mongodb.net/Convertisseur/",
-  mongoURI: "mongodb+srv://Sali:ikWfoyTulzmdYZhc@atlascluster.2bzkbwe.mongodb.net/Convertisseur/",
-};
+export const mongoURI = "mongodb+srv://Sali:ikWfoyTulzmdYZhc@atlascluster.2bzkbwe.mongodb.net/Convertisseur/";
+//export const mongoURI = "mongodb+srv://Client:uZKWfX6J0aejgnst@atlascluster.2bzkbwe.mongodb.net/Convertisseur/";
   
